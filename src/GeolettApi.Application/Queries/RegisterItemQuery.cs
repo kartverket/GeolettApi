3af310b990ed6a15ace740a68b518347ba1e7d0a
Dag Olav Dahle <dagolav@arkitektum.no>
2022-04-07 17:31:09 +0200

    Only display valid items if not logged in

--- src/GeolettApi.Application/Queries/RegisterItemQuery.cs
+++ src/GeolettApi.Application/Queries/RegisterItemQuery.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
+using GeolettApi.Application.Services.Authorization.GeoID;
 
 namespace GeolettApi.Application.Queries
 {
@@ -13,13 +14,16 @@ namespace GeolettApi.Application.Queries
     {
         private readonly GeolettContext _context;
         private readonly IViewModelMapper<RegisterItem, RegisterItemViewModel, Geolett> _registerItemViewModelMapper;
+        private readonly IGeoIDService _geoIDService;
 
         public RegisterItemQuery(
             GeolettContext context,
-            IViewModelMapper<RegisterItem, RegisterItemViewModel, Geolett> registerItemViewModelMapper)
+            IViewModelMapper<RegisterItem, RegisterItemViewModel, Geolett> registerItemViewModelMapper,
+            IGeoIDService geoIDService)
         {
             _context = context;
             _registerItemViewModelMapper = registerItemViewModelMapper;
+            _geoIDService = geoIDService;
         }
 
         public async Task<List<RegisterItemViewModel>> GetAllInternalAsync()
@@ -42,7 +46,12 @@ namespace GeolettApi.Application.Queries
             var viewModels = registerItems
                 .ConvertAll(registerItem => _registerItemViewModelMapper.MapToViewModel(registerItem));
 
-            return viewModels.OrderBy(o => o.ContextType).ToList();
+            var user = await _geoIDService.GetUser();
+
+            if (user == null)
+                viewModels = viewModels.Where(v => v.Status == Status.Valid).ToList();
+
+                return viewModels.OrderBy(o => o.ContextType).ToList();
         }
 
         public async Task<List<Geolett>> GetAllAsync()
@@ -63,6 +72,11 @@ namespace GeolettApi.Application.Queries
 
             var viewModels = registerItems
                 .ConvertAll(registerItem => _registerItemViewModelMapper.MapToGeolett(registerItem));
+
+            var user = await _geoIDService.GetUser();
+
+            if (user == null)
+                viewModels = viewModels.Where(v => v.Status == "Gyldig").ToList();
 
             return viewModels.OrderBy(o => o.KontekstType).ToList();
         }
