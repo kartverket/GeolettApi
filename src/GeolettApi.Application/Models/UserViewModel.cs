6544a29911960a2cf3fc9effcdfaa94c33c317b0
Dag Olav Dahle <dagolav@arkitektum.no>
2025-02-26 15:52:33 +0100

    Add userinfo name and email

--- src/GeolettApi.Application/Models/UserViewModel.cs
+++ src/GeolettApi.Application/Models/UserViewModel.cs
@@ -7,6 +7,8 @@ namespace GeolettApi.Application.Models
     {
         public string OrganizationName { get; set; }
         public long OrganizationNumber { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
         public List<string> Roles { get; set; }
         public bool IsAdmin => Roles?.Contains(GeonorgeRole.Admin) ?? false;
         public bool HasRole(string role) => Roles?.Contains(role) ?? false;
